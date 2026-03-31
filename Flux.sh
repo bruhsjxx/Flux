5442da7a07e1235ed402ad493423fa5a61f5c308
bruhsjxx <myltax25@gmail.com>
2026-04-01 00:50:31 +0300

    Update Flux.sh

--- Flux.sh
+++ Flux.sh
@@ -7,23 +7,21 @@ FLUX_PATH="/workspace/ComfyUI/models/checkpoints/flux_nsfw_bf16.safetensors"
 FLUX_URL="https://civitai.com/api/download/models/2789377?type=Model&format=SafeTensor&size=full&fp=bf16"
 
 # Token kontrolü
-
 if [ -z "$civitai_token" ]; then
-echo "UYARI: civitai_token bulunamadi! Indirme basarisiz olabilir."
+  echo "UYARI: civitai_token bulunamadi! Indirme basarisiz olabilir."
 fi
 
 # Model yoksa indir
-
 if [ ! -f "$FLUX_PATH" ]; then
-echo "FLUX model indiriliyor..."
-mkdir -p /workspace/ComfyUI/models/checkpoints
+  echo "FLUX model indiriliyor..."
+  mkdir -p /workspace/ComfyUI/models/checkpoints
 
-wget --header="Authorization: Bearer $civitai_token" 
--c --progress=bar:force 
--O "$FLUX_PATH" "$FLUX_URL"
+  wget --header="Authorization: Bearer $civitai_token" \
+  -c --progress=bar:force \
+  -O "$FLUX_PATH" "$FLUX_URL"
 
 else
-echo "FLUX zaten mevcut, atlanıyor."
+  echo "FLUX zaten mevcut, atlanıyor."
 fi
 
 echo "===== CUSTOM SETUP END ====="
