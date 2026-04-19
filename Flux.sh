0bfe91470d5131fbd1cad69bfd6aec0aff435c48
bruhsjxx <myltax25@gmail.com>
2026-04-19 06:36:33 +0300

    Add files via upload

---
+++ Flux.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+echo "===== FLUX 2 AUTO SETUP BAŞLADI ====="
+
+# wget kontrolü
+if ! command -v wget &> /dev/null; then
+  if command -v apt-get &> /dev/null; then
+    apt-get update && apt-get install -y wget
+  fi
+fi
+
+BASE_DIR="/workspace/ComfyUI/models"
+
+mkdir -p "$BASE_DIR/checkpoints"
+mkdir -p "$BASE_DIR/clip"
+mkdir -p "$BASE_DIR/vae"
+
+# HF token kontrolü
+if [ -z "$HF_TOKEN" ]; then
+  echo "HATA: HF_TOKEN bulunamadı!"
+  exit 1
+fi
+
+HF_HEADER="--header=Authorization: Bearer $HF_TOKEN"
+
+# =========================
+# 1️⃣ MODEL
+# =========================
+echo "FLUX2 model indiriliyor..."
+wget $HF_HEADER -c "https://huggingface.co/black-forest-labs/FLUX.2-klein-base-9b-fp8/resolve/main/flux-2-klein-base-9b-fp8.safetensors" \
+-O "$BASE_DIR/checkpoints/flux2-klein-base-9b-fp8.safetensors"
+
+# =========================
+# 2️⃣ TEXT ENCODER (QWEN)
+# =========================
+echo "Qwen encoder indiriliyor..."
+wget $HF_HEADER -c "https://huggingface.co/Comfy-Org/flux2-klein-9B/resolve/main/split_files/text_encoders/qwen_3_8b_fp8mixed.safetensors" \
+-O "$BASE_DIR/clip/qwen_3_8b_fp8mixed.safetensors"
+
+# =========================
+# 3️⃣ VAE
+# =========================
+echo "VAE indiriliyor..."
+wget $HF_HEADER -c "https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/split_files/vae/flux2-vae.safetensors" \
+-O "$BASE_DIR/vae/flux2-vae.safetensors"
+
+echo "===== FLUX 2 KURULUM TAMAMLANDI ====="
